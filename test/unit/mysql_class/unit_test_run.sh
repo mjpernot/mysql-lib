4dcf5fc964e81369f1134b1fdc4ed05c3ceb80c9
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-06-28 15:53:36 +0100

    Update unit test files

--- test/unit/mysql_class/unit_test_run.sh
+++ test/unit/mysql_class/unit_test_run.sh
@@ -5,5 +5,13 @@
 #   is located at.
 
 echo "Unit test: mysql_class"  
+test/unit/mysql_class/fetch_global_var.py
+test/unit/mysql_class/fetch_sys_var.py
+test/unit/mysql_class/flush_logs.py
+test/unit/mysql_class/show_master_stat.py
+test/unit/mysql_class/show_slave_hosts.py
+test/unit/mysql_class/show_slave_stat.py
+test/unit/mysql_class/slave_start.py
+test/unit/mysql_class/slave_stop.py
 test/unit/mysql_class/Server_init.py
 
