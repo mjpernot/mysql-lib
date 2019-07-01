04280dabb5810a3a99bee18d7fbf831b8c4f4218
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-07-01 09:42:22 +0000

    Add SlaveRep_startslave.py file

---
+++ test/unit/mysql_class/SlaveRep_startslave.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  SlaveRep_startslave.py
+
+    Description:  Unit testing of Server.start_slave in mysql_class.py.
+
+    Usage:
+        test/unit/mysql_class/SlaveRep_startslave.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import mysql_class
+import version
+
+__version__ = version.__version__
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_value -> Test with values returned.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.name = "Mysql_Server"
+        self.server_id = 10
+        self.sql_user = "mysql_user"
+        self.sql_pass = "my_pwd"
+        self.machine = "Linux"
+        self.host = "host_server"
+        self.port = 3307
+        self.defaults_file = "def_cfg_file"
+        self.extra_def_file = "extra_cfg_file"
+
+        self.show_stat = [
+            {"Variable_name": "io_state", "Value" : "Up"},
+            {"Variable_name": "secs_behind", "Value" : 0}]
+
+    @mock.patch("mysql_class.slave_start")
+    @mock.patch("mysql_class.show_slave_stat")
+    def test_value(self, mock_stat, mock_start):
+
+        """Function:  test_value
+
+        Description:  Test with values returned.
+
+        Arguments:
+
+        """
+
+        mock_start.return_value = True
+        mock_stat.return_value = self.show_stat
+        mysqlrep = mysql_class.Server(self.name, self.server_id, self.sql_user,
+                                      self.sql_pass, self.machine,
+                                      defaults_file=self.defaults_file)
+
+        mysqlrep.start_slave()
+        self.assertEqual((mysqlrep.io_state, mysqlrep.secs_behind),
+                         ("Up", 0))
+
+
+if __name__ == "__main__":
+    unittest.main()
