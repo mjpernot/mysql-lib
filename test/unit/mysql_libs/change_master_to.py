da54fc3a0946c42180cc9c94dd32843f0e3eca9a
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-07-02 19:06:41 +0100

    Add test/unit/mysql_libs/change_master_to.py

---
+++ test/unit/mysql_libs/change_master_to.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  change_master_to.py
+
+    Description:  Unit testing of change_master_to in mysql_libs.py.
+
+    Usage:
+        test/unit/mysql_libs/change_master_to.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+
+# Local
+sys.path.append(os.getcwd())
+import lib.gen_libs as gen_libs
+import mysql_libs
+import version
+
+__version__ = version.__version__
+
+
+class Server(object):
+
+    """Class:  Server
+
+    Description:  Class stub holder for Server class.
+
+    Super-Class:  None
+
+    Sub-Classes:  None
+
+    Methods:
+        __init__ -> Class initialization.
+        sql -> Stub holder for Server.sql method.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+            None
+
+        """
+
+        self.gtid_mode = True
+        self.host = "hostname"
+        self.port = 3306
+        self.sql_user = "sqluser"
+        self.sql_pass = "sqluserpwd"
+        self.file = "binlog-filename"
+        self.pos = "file-position"
+        self.name = "server-name"
+
+    def sql(self, res_set, cmd, var):
+
+        """Method:  sql
+
+        Description:  Stub holder for Server.sql method.
+
+        Arguments:
+            (input) cmd -> Query command.
+            (input) res_set -> Result returning format.
+            (input) params -> Param set.
+
+        """
+
+        return True
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_change_master_to -> Test change_master_to function.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.Master = Server()
+        self.Slave = Server()
+
+    def test_change_master_to(self):
+
+        """Function:  test_change_master_to
+
+        Description:  Test change_master_to function.
+
+        Arguments:
+
+        """
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mysql_libs.change_master_to(self.Master,
+                                                         self.Slave))
+
+
+if __name__ == "__main__":
+    unittest.main()
