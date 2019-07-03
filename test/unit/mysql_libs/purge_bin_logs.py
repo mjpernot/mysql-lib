356c9b8d28150729947443e08c9b1f9b03b86a76
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-07-03 07:15:42 +0000

    Add purge_bin_logs.py file

---
+++ test/unit/mysql_libs/purge_bin_logs.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  purge_bin_logs.py
+
+    Description:  Unit testing of purge_bin_logs in mysql_libs.py.
+
+    Usage:
+        test/unit/mysql_libs/purge_bin_logs.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+
+# Local
+sys.path.append(os.getcwd())
+import mysql_libs
+import version
+
+__version__ = version.__version__
+
+
+class Server(object):
+
+    """Class:  Server
+
+    Description:  Class stub holder for Server class.
+
+    Super-Class:  None
+
+    Sub-Classes:  None
+
+    Methods:
+        __init__ -> Class initialization.
+        sql -> Stub holder for mysql_class.Server.sql method.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+            None
+
+        """
+
+        pass
+
+    def sql(self, cmd, var):
+
+        """Method:  sql
+
+        Description:  Stub holder for mysql_class.Server.sql method.
+
+        Arguments:
+            (input) cmd -> Query command.
+            (input) var -> Global variable name.
+
+        """
+
+        return True
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_purge_bin_logs -> Test purge_bin_logs function.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.Server = Server()
+
+    def test_purge_bin_logs(self):
+
+        """Function:  test_purge_bin_logs
+
+        Description:  Test purge_bin_logs function.
+
+        Arguments:
+
+        """
+
+        self.assertTrue(mysql_libs.purge_bin_logs(self.Server, "TO",
+                                                  "Filename"))
+
+
+if __name__ == "__main__":
+    unittest.main()
