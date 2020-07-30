b58b54ed38aebd3f0abb4878f17d70b9db7568a3
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-30 10:37:00 +0100

    Updated test/unit/mysql_class/slaverep_connect.py v5.0.0

--- test/unit/mysql_class/slaverep_connect.py
+++ test/unit/mysql_class/slaverep_connect.py
@@ -29,6 +29,7 @@ import mock
 # Local
 sys.path.append(os.getcwd())
 import mysql_class
+import lib.machine as machine
 import version
 
 __version__ = version.__version__
@@ -61,7 +62,7 @@ class UnitTest(unittest.TestCase):
         self.server_id = 10
         self.sql_user = "mysql_user"
         self.sql_pass = "my_pwd"
-        self.machine = "Linux"
+        self.machine = getattr(machine, "Linux")()
         self.host = "host_server"
         self.port = 3307
         self.defaults_file = "def_cfg_file"
@@ -69,7 +70,7 @@ class UnitTest(unittest.TestCase):
 
         self.mysqlrep = mysql_class.SlaveRep(
             self.name, self.server_id, self.sql_user, self.sql_pass,
-            machine=self.machine, defaults_file=self.defaults_file)
+            os_type=self.machine, defaults_file=self.defaults_file)
 
     @mock.patch("mysql_class.SlaveRep.upd_slv_status")
     @mock.patch("mysql_class.Server.set_srv_gtid")
