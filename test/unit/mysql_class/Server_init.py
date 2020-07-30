813afa08679375d85d576b9a1a4b3948bd61257f
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-30 10:26:12 +0100

    Updated test/unit/mysql_class/Server_init.py v5.0.0

--- test/unit/mysql_class/Server_init.py
+++ test/unit/mysql_class/Server_init.py
@@ -28,6 +28,7 @@ else:
 # Local
 sys.path.append(os.getcwd())
 import mysql_class
+import lib.machine as machine
 import version
 
 __version__ = version.__version__
@@ -59,31 +60,48 @@ class UnitTest(unittest.TestCase):
         self.server_id = 10
         self.sql_user = "mysql_user"
         self.sql_pass = "my_pwd"
-        self.machine = "Linux"
+        self.machine = getattr(machine, "Linux")()
         self.host = "host_server"
         self.port = 3307
         self.defaults_file = "def_cfg_file"
         self.extra_def_file = "extra_cfg_file"
+        self.results = self.machine.defaults_file
 
-    def test_default(self):
+    def test_no_default(self):
 
-        """Function:  test_default
+        """Function:  test_no_default
 
-        Description:  Test __init__ method with default arguments.
+        Description:  Test with no default file.
 
         Arguments:
 
         """
 
-        mysqldb = mysql_class.Server(self.name, self.server_id, self.sql_user,
-                                     self.sql_pass, self.machine,
-                                     defaults_file=self.defaults_file)
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine)
 
-        self.assertEqual((mysqldb.name, mysqldb.server_id, mysqldb.sql_user,
-                          mysqldb.sql_pass, mysqldb.machine, mysqldb.host,
-                          mysqldb.port),
-                         (self.name, self.server_id, self.sql_user,
-                          self.sql_pass, self.machine, "localhost", 3306))
+        self.assertEqual(mysqldb.defaults_file, self.results)
+
+    def test_default(self):
+
+        """Function:  test_default
+
+        Description:  Test with default file.
+
+        Arguments:
+
+        """
+
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine, defaults_file=self.defaults_file)
+
+        self.assertEqual(
+            (mysqldb.name, mysqldb.server_id, mysqldb.sql_user,
+             mysqldb.sql_pass, mysqldb.machine, mysqldb.host, mysqldb.port),
+            (self.name, self.server_id, self.sql_user, self.sql_pass,
+             self.machine, "localhost", 3306))
 
 
 if __name__ == "__main__":
