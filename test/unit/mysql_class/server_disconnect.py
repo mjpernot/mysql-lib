92cabe7432be28ec025b25acd3f5d35977a2fd69
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-27 10:38:37 +0100

    Updated test/unit/mysql_class/server_disconnect.py v5.0.0

--- test/unit/mysql_class/server_disconnect.py
+++ test/unit/mysql_class/server_disconnect.py
@@ -24,12 +24,11 @@ else:
     import unittest
 
 # Third-party
-import mock
 
 # Local
 sys.path.append(os.getcwd())
 import mysql_class
-import lib.gen_libs as gen_libs
+import lib.machine as machine
 import version
 
 __version__ = version.__version__
@@ -94,6 +93,16 @@ class UnitTest(unittest.TestCase):
 
         """
 
+        self.name = "Mysql_Server"
+        self.server_id = 10
+        self.sql_user = "mysql_user"
+        self.sql_pass = "my_japd"
+        self.machine = getattr(machine, "Linux")()
+        self.host = "host_server"
+        self.port = 3307
+        self.defaults_file = "def_cfg_file"
+        self.extra_def_file = "extra_cfg_file"
+
         self.conn = Server()
 
     def test_disconnect(self):
@@ -106,7 +115,11 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        mysqldb = self.conn
+        mysqldb = mysql_class.Server(self.name, self.server_id, self.sql_user,
+                                     self.sql_pass, self.machine,
+                                     defaults_file=self.defaults_file)
+
+        mysqldb.conn = self.conn
 
         self.assertFalse(mysqldb.disconnect())
 
