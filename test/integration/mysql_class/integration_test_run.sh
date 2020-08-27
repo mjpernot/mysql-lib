f48919f63e12b5c438a04f1d2c6048114e72b1e8
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-27 15:48:38 +0100

    Added integration test files v5.0.0

---
+++ test/integration/mysql_class/integration_test_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Integration testing program for the module.
+# This will run all the integration tests for this program.
+# Will need to run this from the base directory where the module file 
+#   is located at.
+
+echo "Unit test: mysql_class"  
+test/integration/mysql_class/fetch_global_var.py
+
