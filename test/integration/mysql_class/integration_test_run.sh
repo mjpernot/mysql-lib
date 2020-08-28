34c7ba7f0c505cd7bb4efa002afa94256b9edba0
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-28 09:26:06 +0100

    Updated test/integration/mysql_class/integration_test_run.sh v5.0.0

--- test/integration/mysql_class/integration_test_run.sh
+++ test/integration/mysql_class/integration_test_run.sh
@@ -9,4 +9,5 @@ test/integration/mysql_class/fetch_global_var.py
 test/integration/mysql_class/fetch_sys_var.py
 test/integration/mysql_class/server_init.py
 test/integration/mysql_class/server_set_srv_binlog_crc.py
+test/integration/mysql_class/server_set_srv_gtid.py
 
