66075fe7dcf232568c555e18ff88853900e40f45
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-31 15:50:28 +0100

    Updated test/integration/mysql_class/masterrep_init.py v5.0.0

--- test/integration/mysql_class/masterrep_init.py
+++ test/integration/mysql_class/masterrep_init.py
@@ -69,7 +69,7 @@ class UnitTest(unittest.TestCase):
 
         self.base_dir = "test/integration"
         self.config_dir = os.path.join(self.base_dir, "config")
-        self.config_name = "mysql_cfg"
+        self.config_name = "master_mysql_cfg"
         self.cfg = gen_libs.load_module(self.config_name, self.config_dir)
         key1 = "pass"
         key2 = "wd"
