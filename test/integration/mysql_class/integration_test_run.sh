f75299f2537558db1b5dc2b2aad327e62f42b706
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-08-31 10:02:14 +0100

    Updated test/integration/mysql_class/integration_test_run.sh v5.0.0

--- test/integration/mysql_class/integration_test_run.sh
+++ test/integration/mysql_class/integration_test_run.sh
@@ -7,6 +7,7 @@
 echo "Unit test: mysql_class"  
 test/integration/mysql_class/fetch_global_var.py
 test/integration/mysql_class/fetch_sys_var.py
+test/integration/mysql_class/server_chg_db.py
 test/integration/mysql_class/server_connect.py
 test/integration/mysql_class/server_disconnect.py
 test/integration/mysql_class/server_init.py
