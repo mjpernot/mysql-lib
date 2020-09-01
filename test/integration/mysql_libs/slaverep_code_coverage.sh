73be94cf69a000abca88b23a8ae0b0fa75cf6596
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-01 13:09:38 +0100

    Added test/integration/mysql_libs/slaverep_code_coverage.sh v5.0.0

---
+++ test/integration/mysql_libs/slaverep_code_coverage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Integration test code coverage for module.
+# This will run the Python code coverage module against all unit test modules.
+# This will show the amount of code that was tested and which lines of code
+#	that was skipped during the test.
+
+coverage erase
+
+echo ""
+echo "Running unit test modules in conjunction with coverage"
+coverage run -a --source=mysql_libs test/integration/mysql_libs/chg_slv_state.py
+
+echo ""
+echo "Producing code coverage report"
+coverage combine
+coverage report -m
+ 
