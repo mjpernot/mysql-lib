919b23ef4ebfdc02dbf503226e9a1928a7dc2c1d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-01 11:59:55 +0100

    Updated test/integration/mysql_class/slavecode_coverage.sh v5.0.0

--- test/integration/mysql_class/slavecode_coverage.sh
+++ test/integration/mysql_class/slavecode_coverage.sh
@@ -46,6 +46,7 @@ coverage run -a --source=mysql_class test/integration/mysql_class/slaverep_conne
 coverage run -a --source=mysql_class test/integration/mysql_class/slaverep_get_err_stat.py
 coverage run -a --source=mysql_class test/integration/mysql_class/slaverep_get_log_info.py
 coverage run -a --source=mysql_class test/integration/mysql_class/slaverep_get_thr_stat.py
+coverage run -a --source=mysql_class test/integration/mysql_class/slaverep_get_time.py
 coverage run -a --source=mysql_class test/integration/mysql_class/slaverep_init.py
 coverage run -a --source=mysql_class test/integration/mysql_class/slaverep_is_slave_up.py
 coverage run -a --source=mysql_class test/integration/mysql_class/slaverep_is_slv_error.py
