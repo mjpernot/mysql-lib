b8732aa17fbb33ec38decb285a00afb8b1bfcbd7
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-01 12:25:10 +0100

    Updated test/integration/mysql_class/slave_integration_test_run.sh v5.0.0

--- test/integration/mysql_class/slave_integration_test_run.sh
+++ test/integration/mysql_class/slave_integration_test_run.sh
@@ -42,6 +42,7 @@ test/integration/mysql_class/show_slave_hosts.py
 test/integration/mysql_class/slaverep_connect.py
 test/integration/mysql_class/slaverep_get_err_stat.py
 test/integration/mysql_class/slaverep_get_log_info.py
+test/integration/mysql_class/slaverep_get_others.py
 test/integration/mysql_class/slaverep_get_thr_stat.py
 test/integration/mysql_class/slaverep_get_time.py
 test/integration/mysql_class/slaverep_init.py
