d3e92423d461f0acc6f816890aeea7b29e2ecdfb
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-01 14:26:34 +0100

    Updated test/integration/mysql_class/slaverep_integration_test_run.sh v5.0.0

--- test/integration/mysql_class/slaverep_integration_test_run.sh
+++ test/integration/mysql_class/slaverep_integration_test_run.sh
@@ -8,6 +8,7 @@ echo "Unit test: mysql_class"
 test/integration/mysql_class/fetch_global_var.py
 test/integration/mysql_class/fetch_sys_var.py
 test/integration/mysql_class/flush_logs.py
+test/integration/mysql_class/gtidset.py
 test/integration/mysql_class/masterrep_connect.py
 test/integration/mysql_class/masterrep_init.py
 test/integration/mysql_class/masterrep_get_log_info.py
