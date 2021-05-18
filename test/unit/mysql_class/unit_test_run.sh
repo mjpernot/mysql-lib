c0f4c1d4db98afbee48d41030e79a2c20fc702a3
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-05-18 09:59:51 +0100

    Updated test/unit/mysql_class/unit_test_run.sh v5.2.0

--- test/unit/mysql_class/unit_test_run.sh
+++ test/unit/mysql_class/unit_test_run.sh
@@ -51,6 +51,7 @@ test/unit/mysql_class/server_reconnect.py
 test/unit/mysql_class/server_set_pass_config.py
 test/unit/mysql_class/server_setsrvbinlogcrc.py
 test/unit/mysql_class/server_setsrvgtid.py
+test/unit/mysql_class/server_setup_ssl.py
 test/unit/mysql_class/server_updlogstats.py
 test/unit/mysql_class/server_updmstrepstat.py
 test/unit/mysql_class/server_updsrvperf.py
