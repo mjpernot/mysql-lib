80d42ea76e8f3de5673417bdd950f390f0aba20c
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-05-26 14:24:20 +0100

    Updated test/integration/mysql_class/server_init.py v5.2.0

--- test/integration/mysql_class/server_init.py
+++ test/integration/mysql_class/server_init.py
@@ -100,6 +100,7 @@ class UnitTest(unittest.TestCase):
         self.config3["ssl_key"] = "KeyFile"
         self.config3["ssl_cert"] = "CertFile"
         self.config3["client_flags"] = [mysql.connector.ClientFlag.SSL]
+        self.config3["ssl_ca"] = ""
         self.config4[key1 + key2] = self.cfg.japd
         self.config4["ssl_ca"] = "CAFile"
         self.config4["ssl_key"] = "KeyFile"
