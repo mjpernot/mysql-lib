f3f6180649b5549fa562f5bdb69caff5a9461ef0
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-06-15 11:12:08 +0100

    Updated test/unit/mysql_class/server_set_ssl_config.py v5.2.0

--- test/unit/mysql_class/server_set_ssl_config.py
+++ test/unit/mysql_class/server_set_ssl_config.py
@@ -43,9 +43,14 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_key_cert_yes_ca6 -> Test with cert and key present and with ca.
+        test_key_cert_yes_ca5 -> Test with cert and key present and with ca.
+        test_key_cert_yes_ca4 -> Test with cert and key present and with ca.
         test_key_cert_yes_ca3 -> Test with cert and key present and with ca.
         test_key_cert_yes_ca2 -> Test with cert and key present and with ca.
         test_key_cert_yes_ca -> Test with cert and key present and with ca.
+        test_key_cert_no_ca5 -> Test with cert and key present, but no ca.
+        test_key_cert_no_ca4 -> Test with cert and key present, but no ca.
         test_key_cert_no_ca3 -> Test with cert and key present, but no ca.
         test_key_cert_no_ca2 -> Test with cert and key present, but no ca.
         test_key_cert_no_ca -> Test with cert and key present, but no ca.
@@ -54,9 +59,14 @@ class UnitTest(unittest.TestCase):
         test_ssl_client_cert -> Test with ssl_client_cert only.
         test_ssl_client_key -> Test with ssl_client_key only.
         test_ssl_client_flag -> Test with ssl_client_flag present.
+        test_ssl_client_key_cert5 -> Test with both cert and key present.
+        test_ssl_client_key_cert4 -> Test with both cert and key present.
         test_ssl_client_key_cert3 -> Test with both cert and key present.
         test_ssl_client_key_cert2 -> Test with both cert and key present.
         test_ssl_client_key_cert -> Test with both cert and key present.
+        test_ssl_client_ca6 -> Test with ssl_client_ca only present.
+        test_ssl_client_ca5 -> Test with ssl_client_ca only present.
+        test_ssl_client_ca4 -> Test with ssl_client_ca only present.
         test_ssl_client_ca3 -> Test with ssl_client_ca only present.
         test_ssl_client_ca2 -> Test with ssl_client_ca only present.
         test_ssl_client_ca -> Test with ssl_client_ca only present.
@@ -88,22 +98,159 @@ class UnitTest(unittest.TestCase):
         self.ssl_client_key = "KeyFile"
         self.ssl_client_cert = "CertFile"
         self.ssl_client_flag = mysql.connector.ClientFlag.SSL
+
         self.config = {}
         self.config[key1 + key2] = self.sql_pass
         self.config["ssl_ca"] = "CAFile"
         self.config["client_flags"] = [mysql.connector.ClientFlag.SSL]
+        self.config["ssl_disabled"] = False
+        self.config["ssl_verify_identity"] = False
+        self.config["ssl_verify_cert"] = False
+
         self.config2 = {}
         self.config2[key1 + key2] = self.sql_pass
         self.config2["ssl_key"] = "KeyFile"
         self.config2["ssl_cert"] = "CertFile"
         self.config2["client_flags"] = [mysql.connector.ClientFlag.SSL]
         self.config2["ssl_ca"] = ""
+        self.config2["ssl_disabled"] = False
+        self.config2["ssl_verify_identity"] = False
+
         self.config3 = {}
         self.config3[key1 + key2] = self.sql_pass
         self.config3["ssl_ca"] = "CAFile"
         self.config3["ssl_key"] = "KeyFile"
         self.config3["ssl_cert"] = "CertFile"
         self.config3["client_flags"] = [mysql.connector.ClientFlag.SSL]
+        self.config3["ssl_disabled"] = False
+        self.config3["ssl_verify_identity"] = False
+        self.config3["ssl_verify_cert"] = False
+
+        self.config4 = {}
+        self.config4[key1 + key2] = self.sql_pass
+        self.config4["ssl_ca"] = "CAFile"
+        self.config4["client_flags"] = [mysql.connector.ClientFlag.SSL]
+        self.config4["ssl_disabled"] = True
+        self.config4["ssl_verify_identity"] = False
+        self.config4["ssl_verify_cert"] = False
+
+        self.config5 = {}
+        self.config5[key1 + key2] = self.sql_pass
+        self.config5["ssl_ca"] = "CAFile"
+        self.config5["client_flags"] = [mysql.connector.ClientFlag.SSL]
+        self.config5["ssl_disabled"] = False
+        self.config5["ssl_verify_identity"] = True
+        self.config5["ssl_verify_cert"] = False
+
+        self.config6 = {}
+        self.config6[key1 + key2] = self.sql_pass
+        self.config6["ssl_ca"] = "CAFile"
+        self.config6["client_flags"] = [mysql.connector.ClientFlag.SSL]
+        self.config6["ssl_disabled"] = False
+        self.config6["ssl_verify_identity"] = False
+        self.config6["ssl_verify_cert"] = True
+
+        self.config7 = {}
+        self.config7[key1 + key2] = self.sql_pass
+        self.config7["ssl_key"] = "KeyFile"
+        self.config7["ssl_cert"] = "CertFile"
+        self.config7["client_flags"] = [mysql.connector.ClientFlag.SSL]
+        self.config7["ssl_ca"] = ""
+        self.config7["ssl_disabled"] = True
+        self.config7["ssl_verify_identity"] = False
+
+        self.config8 = {}
+        self.config8[key1 + key2] = self.sql_pass
+        self.config8["ssl_key"] = "KeyFile"
+        self.config8["ssl_cert"] = "CertFile"
+        self.config8["client_flags"] = [mysql.connector.ClientFlag.SSL]
+        self.config8["ssl_ca"] = ""
+        self.config8["ssl_disabled"] = False
+        self.config8["ssl_verify_identity"] = True
+
+        self.config9 = {}
+        self.config9[key1 + key2] = self.sql_pass
+        self.config9["ssl_ca"] = "CAFile"
+        self.config9["ssl_key"] = "KeyFile"
+        self.config9["ssl_cert"] = "CertFile"
+        self.config9["client_flags"] = [mysql.connector.ClientFlag.SSL]
+        self.config9["ssl_disabled"] = True
+        self.config9["ssl_verify_identity"] = False
+        self.config9["ssl_verify_cert"] = False
+
+        self.config10 = {}
+        self.config10[key1 + key2] = self.sql_pass
+        self.config10["ssl_ca"] = "CAFile"
+        self.config10["ssl_key"] = "KeyFile"
+        self.config10["ssl_cert"] = "CertFile"
+        self.config10["client_flags"] = [mysql.connector.ClientFlag.SSL]
+        self.config10["ssl_disabled"] = False
+        self.config10["ssl_verify_identity"] = True
+        self.config10["ssl_verify_cert"] = False
+
+        self.config11 = {}
+        self.config11[key1 + key2] = self.sql_pass
+        self.config11["ssl_ca"] = "CAFile"
+        self.config11["ssl_key"] = "KeyFile"
+        self.config11["ssl_cert"] = "CertFile"
+        self.config11["client_flags"] = [mysql.connector.ClientFlag.SSL]
+        self.config11["ssl_disabled"] = False
+        self.config11["ssl_verify_identity"] = False
+        self.config11["ssl_verify_cert"] = True
+
+    def test_key_cert_yes_ca6(self):
+
+        """Function:  test_key_cert_yes_ca6
+
+        Description:  Test with cert and key present and with ca.
+
+        Arguments:
+
+        """
+
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine, ssl_client_key=self.ssl_client_key,
+            ssl_client_cert=self.ssl_client_cert,
+            ssl_client_ca=self.ssl_client_ca, ssl_verify_cert=True)
+
+        self.assertEqual(mysqldb.config, self.config11)
+
+    def test_key_cert_yes_ca5(self):
+
+        """Function:  test_key_cert_yes_ca5
+
+        Description:  Test with cert and key present and with ca.
+
+        Arguments:
+
+        """
+
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine, ssl_client_key=self.ssl_client_key,
+            ssl_client_cert=self.ssl_client_cert,
+            ssl_client_ca=self.ssl_client_ca, ssl_verify_id=True)
+
+        self.assertEqual(mysqldb.config, self.config10)
+
+    def test_key_cert_yes_ca4(self):
+
+        """Function:  test_key_cert_yes_ca4
+
+        Description:  Test with cert and key present and with ca.
+
+        Arguments:
+
+        """
+
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine, ssl_client_key=self.ssl_client_key,
+            ssl_client_cert=self.ssl_client_cert,
+            ssl_client_ca=self.ssl_client_ca, ssl_disabled=True)
+
+        self.assertEqual(mysqldb.config, self.config9)
 
     def test_key_cert_yes_ca3(self):
 
@@ -163,6 +310,40 @@ class UnitTest(unittest.TestCase):
             (mysqldb.ssl_client_key, mysqldb.ssl_client_cert),
             (self.ssl_client_key, self.ssl_client_cert))
 
+    def test_key_cert_no_ca5(self):
+
+        """Function:  test_key_cert_no_ca5
+
+        Description:  Test with cert and key present, but no ca.
+
+        Arguments:
+
+        """
+
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine, ssl_client_key=self.ssl_client_key,
+            ssl_client_cert=self.ssl_client_cert, ssl_verify_id=True)
+
+        self.assertEqual(mysqldb.config, self.config8)
+
+    def test_key_cert_no_ca4(self):
+
+        """Function:  test_key_cert_no_ca4
+
+        Description:  Test with cert and key present, but no ca.
+
+        Arguments:
+
+        """
+
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine, ssl_client_key=self.ssl_client_key,
+            ssl_client_cert=self.ssl_client_cert, ssl_disabled=True)
+
+        self.assertEqual(mysqldb.config, self.config7)
+
     def test_key_cert_no_ca3(self):
 
         """Function:  test_key_cert_no_ca3
@@ -179,7 +360,6 @@ class UnitTest(unittest.TestCase):
             ssl_client_cert=self.ssl_client_cert)
 
         self.assertEqual(mysqldb.config, self.config2)
-
 
     def test_key_cert_no_ca2(self):
 
@@ -309,6 +489,40 @@ class UnitTest(unittest.TestCase):
 
         self.assertEqual(mysqldb.ssl_client_flag, self.ssl_client_flag)
 
+    def test_ssl_client_key_cert5(self):
+
+        """Function:  test_ssl_client_key_cert5
+
+        Description:  Test with both cert and key present.
+
+        Arguments:
+
+        """
+
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine, ssl_client_key=self.ssl_client_key,
+            ssl_client_cert=self.ssl_client_cert, ssl_verify_id=True)
+
+        self.assertEqual(mysqldb.config, self.config8)
+
+    def test_ssl_client_key_cert4(self):
+
+        """Function:  test_ssl_client_key_cert4
+
+        Description:  Test with both cert and key present.
+
+        Arguments:
+
+        """
+
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine, ssl_client_key=self.ssl_client_key,
+            ssl_client_cert=self.ssl_client_cert, ssl_disabled=True)
+
+        self.assertEqual(mysqldb.config, self.config7)
+
     def test_ssl_client_key_cert3(self):
 
         """Function:  test_ssl_client_key_cert3
@@ -325,7 +539,6 @@ class UnitTest(unittest.TestCase):
             ssl_client_cert=self.ssl_client_cert)
 
         self.assertEqual(mysqldb.config, self.config2)
-
 
     def test_ssl_client_key_cert2(self):
 
@@ -365,9 +578,60 @@ class UnitTest(unittest.TestCase):
             (mysqldb.ssl_client_key, mysqldb.ssl_client_cert),
             (self.ssl_client_key, self.ssl_client_cert))
 
+    def test_ssl_client_ca6(self):
+
+        """Function:  test_ssl_client_ca6
+
+        Description:  Test with ssl_client_ca only present.
+
+        Arguments:
+
+        """
+
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine, ssl_client_ca=self.ssl_client_ca,
+            ssl_verify_cert=True)
+
+        self.assertEqual(mysqldb.config, self.config6)
+
+    def test_ssl_client_ca5(self):
+
+        """Function:  test_ssl_client_ca5
+
+        Description:  Test with ssl_client_ca only present.
+
+        Arguments:
+
+        """
+
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine, ssl_client_ca=self.ssl_client_ca,
+            ssl_verify_id=True)
+
+        self.assertEqual(mysqldb.config, self.config5)
+
+    def test_ssl_client_ca4(self):
+
+        """Function:  test_ssl_client_ca4
+
+        Description:  Test with ssl_client_ca only present.
+
+        Arguments:
+
+        """
+
+        mysqldb = mysql_class.Server(
+            self.name, self.server_id, self.sql_user, self.sql_pass,
+            os_type=self.machine, ssl_client_ca=self.ssl_client_ca,
+            ssl_disabled=True)
+
+        self.assertEqual(mysqldb.config, self.config4)
+
     def test_ssl_client_ca3(self):
 
-        """Function:  test_ssl_client_flag
+        """Function:  test_ssl_client_ca3
 
         Description:  Test with ssl_client_ca only present.
 
@@ -383,7 +647,7 @@ class UnitTest(unittest.TestCase):
 
     def test_ssl_client_ca2(self):
 
-        """Function:  test_ssl_client_flag2
+        """Function:  test_ssl_client_ca2
 
         Description:  Test with ssl_client_ca only present.
 
@@ -402,7 +666,7 @@ class UnitTest(unittest.TestCase):
 
     def test_ssl_client_ca(self):
 
-        """Function:  test_ssl_client_flag
+        """Function:  test_ssl_client_ca
 
         Description:  Test with ssl_client_ca only present.
 
